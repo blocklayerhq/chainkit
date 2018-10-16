73d5bd7ae38384b129f8bff76871622890f5c9cf
Andrea Luzzardi <aluzzardi@gmail.com>
2018-10-16 12:25:25 -0700

    templating support
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- cmd/init.go
+++ cmd/init.go
@@ -1,16 +1,22 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path"
+	"text/template"
 
 	_ "github.com/blocklayerhq/chainkit/templates/build" // embed the static assets
 	"github.com/rakyll/statik/fs"
 	"github.com/spf13/cobra"
 )
+
+type templateContext struct {
+	Name string
+}
 
 var initCmd = &cobra.Command{
 	Use:   "init <name>",
@@ -29,31 +35,45 @@ var initCmd = &cobra.Command{
 }
 
 func initialize(name, dest string) error {
+	// Make sure the destination path doesn't exist.
+	if _, err := os.Stat(dest); !os.IsNotExist(err) {
+		return fmt.Errorf("destination path %q already exists", dest)
+	}
+
+	ctx := &templateContext{
+		Name: name,
+	}
+
 	templates, err := fs.New()
 	if err != nil {
 		return err
 	}
 
-	if err := extractFiles(templates, dest); err != nil {
+	if err := extractFiles(ctx, templates, dest); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func extractFiles(templates http.FileSystem, dest string) error {
+func extractFiles(ctx *templateContext, templates http.FileSystem, dest string) error {
 	err := fs.Walk(templates, "/", func(path string, fi os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		return extractFile(templates, path, dest, fi)
+		return extractFile(ctx, templates, path, dest, fi)
 	})
 	return err
 }
 
-func extractFile(templates http.FileSystem, src, dst string, fi os.FileInfo) error {
-	dstPath := path.Join(dst, src)
-	fmt.Println(dstPath)
+func extractFile(ctx *templateContext, templates http.FileSystem, src, dst string, fi os.FileInfo) error {
+	// Templatize the file name.
+	parsedSrc, err := templatize(ctx, src)
+	if err != nil {
+		return err
+	}
+
+	dstPath := path.Join(dst, string(parsedSrc))
 
 	if fi.IsDir() {
 		return os.MkdirAll(dstPath, fi.Mode())
@@ -63,7 +83,24 @@ func extractFile(templates http.FileSystem, src, dst string, fi os.FileInfo) err
 	if err != nil {
 		return err
 	}
-	return ioutil.WriteFile(dstPath, data, fi.Mode())
+	output, err := templatize(ctx, string(data))
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(dstPath, output, fi.Mode())
+}
+
+func templatize(ctx *templateContext, input string) ([]byte, error) {
+	t, err := template.New("chainkit").Parse(input)
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	if err := t.Execute(&buf, ctx); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
 func init() {
