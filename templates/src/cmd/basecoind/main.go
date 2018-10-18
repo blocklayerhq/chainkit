304b0926696215f8bbe2f06896e88f7ca960eaee
Andrea Luzzardi <aluzzardi@gmail.com>
2018-10-17 17:33:04 -0700

    templates: Use `basecoin` as a starting point
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

---
+++ templates/src/cmd/basecoind/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	gaiaInit "github.com/cosmos/cosmos-sdk/cmd/gaia/init"
+
+	"{{ .GoPkg }}/app"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/cli"
+	dbm "github.com/tendermint/tendermint/libs/db"
+	"github.com/tendermint/tendermint/libs/log"
+	tmtypes "github.com/tendermint/tendermint/types"
+)
+
+func main() {
+	cdc := app.MakeCodec()
+	ctx := server.NewDefaultContext()
+
+	rootCmd := &cobra.Command{
+		Use:               "basecoind",
+		Short:             "Basecoin Daemon (server)",
+		PersistentPreRunE: server.PersistentPreRunEFn(ctx),
+	}
+
+	appInit := server.DefaultAppInit
+	rootCmd.AddCommand(gaiaInit.InitCmd(ctx, cdc, appInit))
+	rootCmd.AddCommand(gaiaInit.TestnetFilesCmd(ctx, cdc, appInit))
+
+	server.AddCommands(ctx, cdc, rootCmd, appInit,
+		newApp, exportAppStateAndTMValidators)
+
+	// prepare and add flags
+	rootDir := os.ExpandEnv("$HOME/.basecoind")
+	executor := cli.PrepareBaseCmd(rootCmd, "BC", rootDir)
+
+	err := executor.Execute()
+	if err != nil {
+		// Note: Handle with #870
+		panic(err)
+	}
+}
+
+func newApp(logger log.Logger, db dbm.DB, storeTracer io.Writer) abci.Application {
+	return app.NewBasecoinApp(logger, db, baseapp.SetPruning(viper.GetString("pruning")))
+}
+
+func exportAppStateAndTMValidators(logger log.Logger, db dbm.DB, storeTracer io.Writer) (json.RawMessage, []tmtypes.GenesisValidator, error) {
+	bapp := app.NewBasecoinApp(logger, db)
+	return bapp.ExportAppStateAndValidators()
+}
