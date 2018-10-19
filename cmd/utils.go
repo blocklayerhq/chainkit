639acbc9d507e82b902c4098c6dc41e28e783570
Andrea Luzzardi <aluzzardi@gmail.com>
2018-10-18 17:14:03 -0700

    - Improved UX - Generate genesis on `init` - Add `start`
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- cmd/utils.go
+++ cmd/utils.go
@@ -5,22 +5,30 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
+	"strings"
 
+	"github.com/blocklayerhq/chainkit/pkg/ui"
 	"github.com/spf13/cobra"
 )
 
-func getCwd(cmd *cobra.Command) (string, error) {
+func getCwd(cmd *cobra.Command) string {
 	cwd, err := cmd.Flags().GetString("cwd")
 	if err != nil {
-		return "", err
+		ui.Fatal("unable to resolve --cwd: %v", err)
+		return ""
 	}
 	if cwd == "" {
 		cwd, err = os.Getwd()
 		if err != nil {
-			return "", err
+			ui.Fatal("unable to determine current directory: %v", err)
+			return ""
 		}
 	}
-	return filepath.Abs(cwd)
+	abs, err := filepath.Abs(cwd)
+	if err != nil {
+		ui.Fatal("unable to parse %q: %v", cwd, err)
+	}
+	return abs
 }
 
 func goPath() string {
@@ -35,11 +43,40 @@ func goSrc() string {
 	return path.Join(goPath(), "src")
 }
 
+func dockerRun(rootDir, name string, args ...string) error {
+	dataDir := path.Join(rootDir, "data")
+
+	daemonName := name + "d"
+	cliName := name + "cli"
+
+	// -v "${data_dir}/${APP_NAME}d:/root/.${APP_NAME}d"
+	daemonDir := path.Join(dataDir, daemonName)
+	daemonDirContainer := path.Join("/", "root", "."+daemonName)
+
+	// -v "${data_dir}/${APP_NAME}cli:/root/.${APP_NAME}cli"
+	cliDir := path.Join(dataDir, cliName)
+	cliDirContainer := path.Join("/", "root", "."+cliName)
+
+	cmd := []string{
+		"run", "--rm",
+		"-p", "26656:26656",
+		"-p", "26657:26657",
+		"-v", daemonDir + ":" + daemonDirContainer,
+		"-v", cliDir + ":" + cliDirContainer,
+		name + ":latest",
+		daemonName,
+	}
+	cmd = append(cmd, args...)
+
+	return docker(rootDir, cmd...)
+}
+
 func docker(rootDir string, args ...string) error {
 	return run(rootDir, "docker", args...)
 }
 
 func run(rootDir, command string, args ...string) error {
+	ui.Verbose("$ %s %s", command, strings.Join(args, " "))
 	cmd := exec.Command(command)
 	cmd.Args = append([]string{command}, args...)
 	cmd.Stdin = os.Stdin
---
+++ pkg/ui/ui.go
@@ -0,0 +1,66 @@
+package ui
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/ttacon/chalk"
+	"github.com/xlab/treeprint"
+)
+
+func Info(msg string, args ...interface{}) {
+	fmt.Printf("%s %s\n", chalk.Bold.TextStyle(chalk.Blue.Color("==>")), chalk.Bold.TextStyle(fmt.Sprintf(msg, args...)))
+}
+
+func Verbose(msg string, args ...interface{}) {
+	fmt.Println(chalk.Dim.TextStyle(fmt.Sprintf(msg, args...)))
+}
+
+func Success(msg string, args ...interface{}) {
+	fmt.Printf("  %s %s\n", chalk.Bold.TextStyle(chalk.Green.Color("✔")), chalk.Bold.TextStyle(fmt.Sprintf(msg, args...)))
+}
+
+func Error(msg string, args ...interface{}) {
+	fmt.Printf("  %s %s\n", chalk.Bold.TextStyle(chalk.Red.Color("✗")), chalk.Bold.TextStyle(fmt.Sprintf(msg, args...)))
+}
+
+func Fatal(msg string, args ...interface{}) {
+	Error(msg, args...)
+	os.Exit(1)
+}
+
+func Emphasize(msg string) string {
+	return chalk.Bold.TextStyle(chalk.Yellow.Color(msg))
+}
+
+func Tree(p string) error {
+	root := treeprint.New()
+	root.SetValue(p)
+	if err := walk(p, root); err != nil {
+		return err
+	}
+	Verbose(strings.TrimSpace(root.String()))
+	return nil
+}
+func walk(p string, node treeprint.Tree) error {
+	files, err := ioutil.ReadDir(p)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			sub := node.AddBranch(file.Name())
+			if err := walk(path.Join(p, file.Name()), sub); err != nil {
+				return err
+			}
+			continue
+		}
+
+		node.AddNode(file.Name())
+	}
+
+	return nil
+}
