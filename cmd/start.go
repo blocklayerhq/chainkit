f94d7e657ca4adcb20d77d7ed06799179d02902a
Sam Alba <sam.alba@gmail.com>
2018-10-21 19:48:34 +0200

    Added Cosmos Explorer

--- cmd/start.go
+++ cmd/start.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"path/filepath"
+	"time"
 
 	"github.com/blocklayerhq/chainkit/pkg/ui"
 	"github.com/spf13/cobra"
@@ -25,10 +26,29 @@ func init() {
 	rootCmd.AddCommand(startCmd)
 }
 
+func startExplorer(ctx context.Context, name, rootDir string) {
+	cmd := []string{
+		"run", "--rm",
+		"-e", "NGINX_PORT=8080",
+		"--network", "container:" + name,
+		"samalba/cosmos-explorer-localhost" + ":latest",
+	}
+	go func() {
+		// FIXME: wait for the container to be created
+		time.Sleep(3 * time.Second)
+		if err := docker(ctx, rootDir, cmd...); err != nil {
+			ui.Fatal("Failed to start the Explorer: %v", err)
+		}
+	}()
+}
+
 func start(name, rootDir string) {
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
 	ui.Info("Starting %s", name)
-	ui.Success("Application is live at %s", ui.Emphasize("http://localhost:26657/"))
+	ui.Success("Application is live at:     %s", ui.Emphasize("http://localhost:26657/"))
+	ui.Success("Cosmos Explorer is live at: %s", ui.Emphasize("http://localhost:8080/"))
+	defer cancel()
+	startExplorer(ctx, name, rootDir)
 	if err := dockerRun(ctx, rootDir, name, "start"); err != nil {
 		ui.Fatal("Failed to start the application: %v", err)
 	}
