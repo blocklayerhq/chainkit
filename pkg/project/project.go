1ea4fed3a0585f192482a67d78dbdc82362ef2e3
Sam Alba <sam.alba@gmail.com>
2018-11-29 18:22:12 -0800

    Implemented project manifest file chainkit.yml

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,28 +1,69 @@
 package project
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"path"
-	"path/filepath"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
 )
+
+type projectBinaries struct {
+	CLI    string
+	Daemon string
+}
 
 // Project represents a project
 type Project struct {
-	Name    string
-	RootDir string
+	Name     string
+	RootDir  string `yaml:"-"`
+	Binaries *projectBinaries
 }
 
-// Create will create a new project in the given directory.
-func Create(dir, name string) (*Project, error) {
+// ChainkitManifest defines the name of the manifest file
+const ChainkitManifest = "chainkit.yml"
+
+// New will create a new project in the given directory.
+func New(dir, name string) *Project {
 	return &Project{
-		Name:    name,
+		Name: name,
+		Binaries: &projectBinaries{
+			CLI:    name + "cli",
+			Daemon: name + "d",
+		},
 		RootDir: path.Join(dir, name),
-	}, nil
+	}
+}
+
+// Save serializes the project data on disk
+func (p *Project) Save() error {
+	ybuf, err := yaml.Marshal(p)
+	if err != nil {
+		return err
+	}
+	fp, err := os.Create(path.Join(p.RootDir, ChainkitManifest))
+	if err != nil {
+		return err
+	}
+	if _, err = fp.Write(ybuf); err != nil {
+		return err
+	}
+	return nil
 }
 
 // Load will load a project from a given directory
 func Load(dir string) (*Project, error) {
-	return &Project{
-		Name:    filepath.Base(dir),
-		RootDir: dir,
-	}, nil
+	errMsg := fmt.Sprintf("Cannot read manifest \"%s\"", ChainkitManifest)
+	data, err := ioutil.ReadFile(path.Join(dir, ChainkitManifest))
+	if err != nil {
+		return nil, errors.Wrap(err, errMsg)
+	}
+	p := &Project{}
+	if err = yaml.Unmarshal(data, p); err != nil {
+		return nil, errors.Wrap(err, errMsg)
+	}
+	p.RootDir = dir
+	return p, nil
 }
