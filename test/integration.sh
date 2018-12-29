06f15dbad017afcaa1c2edc0cdf5c9562f6e32c8
Sam Alba <sam.alba@gmail.com>
2018-12-29 12:32:53 -0800

    Improved test logging and implemented tests for chainkit join
    Signed-off-by: Sam Alba <sam.alba@gmail.com>

--- test/integration.sh
+++ test/integration.sh
@@ -6,7 +6,7 @@ TMP_DIR=/tmp/chainkit-integration-tests
 PROJECT_NAME=foo
 CMD=""
 
-test_create() {
+1_test_create() {
     $CMD create $PROJECT_NAME
     (
         # Check that key files have been created
@@ -20,7 +20,7 @@ test_create() {
     $CMD create $PROJECT_NAME || true
 }
 
-test_build() {
+2_test_build() {
     # Check that you cannot build outside the project dir
     $CMD build || true
     (
@@ -30,31 +30,51 @@ test_build() {
     )
 }
 
-test_start() {
+3_test_start() {
     $CMD start --cwd $PROJECT_NAME > chainkit-start.log 2>&1 &
     # Give some time for the chain to start
-    retry "curl -s -I -X GET http://localhost:42001 | grep '200 OK'"
+    retry 2 10 "curl -s -I -X GET http://localhost:42001 | grep '200 OK'"
 }
 
-test_cli() {
+4_test_cli() {
     (
         cd $PROJECT_NAME
         $CMD cli status
     )
 }
 
-test_explorer() {
+5_test_explorer() {
     # Test if the explorer container is running
-    retry '[ ! -z $(docker ps -qf label=chainkit.cosmos.explorer) ]'
-    retry 'curl -X GET -I http://localhost:42000'
+    retry 2 10 '[ ! -z $(docker ps -qf label=chainkit.cosmos.explorer) ]'
+    retry 2 10 'curl -X GET -I http://localhost:42000'
+}
+
+6_test_join() {
+    # Check that the first node is successfully registered on the network
+    retry 3 30 'grep "Node successfully registered" chainkit-start.log'
+    network_id=$(grep 'chainkit join' chainkit-start.log | awk '{print $3}')
+    [ ! -z "$network_id" ]
+    $CMD join $network_id > chainkit-join.log 2>&1 &
+    # Check that we discovered the first node
+    retry 3 30 'grep "Discovered node " chainkit-join.log'
+    # Check that our node id has been spotted in the first node's log
+    node_id=$(grep 'Node ID' chainkit-join.log | awk '{print $5}')
+    [ ! -z "$node_id" ]
+    retry 3 30 'grep "Discovered node $node_id" chainkit-start.log'
+    # The 2nd application should be live on port 42011 (port allocation)
+    curl -s -I -X GET http://localhost:42011 | grep '200 OK'
 }
 
 # Retry a command for 20 sec
 retry() {
-    for i in $(seq 1 5) ; do
-        eval "$1" && return || true
-        sleep 4
+    unset_trap
+    for i in $(seq 1 $2) ; do
+        eval "$3" && set_trap && return || true
+        sleep $1
     done
+    # Reset trap before return
+    set_trap
+    echo "!!! Retry failed on command: $3"
     false
 }
 
@@ -68,25 +88,34 @@ cleanup() {
 show_logs() {
     (
         cd ${TMP_DIR}/src
-        tail -n 50 chainkit-start.log
+        tail -n 50 chainkit-*.log
     )
+    echo "!!! FAILED on line $1"
+}
+
+set_trap() {
+    trap 'show_logs $LINENO' ERR SIGHUP SIGINT SIGTERM
+}
+
+unset_trap() {
+    trap - ERR
 }
 
 run_tests() {
     CMD="$1"
     # clear-up tmp-dir if exists
     cleanup
-    trap show_logs ERR SIGHUP SIGINT SIGTERM
+    set_trap
     mkdir -p ${TMP_DIR}/src
     (
         cd ${TMP_DIR}/src
         export GOPATH=$TMP_DIR
+        test_suite=$(typeset -f | awk '/ \(\) $/ && /^[0-9]+_test_/ {print $1}')
         set -x
-        test_create
-        test_build
-        test_start
-        test_cli
-        test_explorer
+        for fn in $test_suite ; do
+            echo "############## Running $fn"
+            $fn
+        done
     )
     echo "All tests passed!"
     cleanup
